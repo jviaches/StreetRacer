d697f776b291a9df00bb3c839e53ed3ef5ceabf3
jviaches <jviaches@gmail.com>
2020-01-20 22:04:25 -0600

    added player controls: keys + android gyro tilt

--- Assets/Scripts/PlayerCar.cs
+++ Assets/Scripts/PlayerCar.cs
@@ -14,11 +14,28 @@ namespace Assets.Scripts
         private Vector3 mainCameraOffset;
 
         private ILevel currentLevel;
+        private Gyroscope gyro;
+
+        private float leftRoadBodundary = -4.5f;
+        private float rigtRoadBodundary = 4.5f;
+        private float sideSpeed = 0.05f;
+
         public float Speed { get; set; }
 
         void Awake()
         {
             currentLevel = GameSettings.GetCurrentLevel();
+        }
+
+        protected void OnGUI()
+        {
+            GUI.skin.label.fontSize = Screen.width / 40;
+
+            GUILayout.Label("Orientation: " + Screen.orientation);
+            GUILayout.Label("input.gyro.attitude x: " + Input.gyro.attitude.x);
+            //GUILayout.Label("input.gyro.attitude y: " + Input.gyro.attitude.y);
+            //GUILayout.Label("input.gyro.attitude z: " + Input.gyro.attitude.z);
+            //GUILayout.Label("input.gyro.attitude w: " + Input.gyro.attitude.w);
         }
 
         void Start()
@@ -27,6 +44,38 @@ namespace Assets.Scripts
 
             mainCamera = Camera.main;
             mainCameraOffset = mainCamera.transform.position - transform.position;
+
+            gyro = Input.gyro;
+            gyro.enabled = true;
+        }
+
+        private void Update()
+        {
+            if (Input.GetKey("left") && transform.position.x > leftRoadBodundary)
+                transform.position = new Vector3(transform.position.x - sideSpeed, 0, 0);
+
+
+            if (Input.GetKey("right") && transform.position.x < rigtRoadBodundary)
+                transform.position = new Vector3(transform.position.x + sideSpeed, 0, 0);
+
+           
+            GyroModifyCamera();
+        }
+
+        // The Gyroscope is right-handed.  Unity is left handed.
+        // Make the necessary change to the camera.
+        void GyroModifyCamera()
+        {
+            if (SystemInfo.supportsGyroscope)
+            {
+                // tilt right
+                if (Input.gyro.attitude.x < -0.05 && transform.position.x < rigtRoadBodundary)
+                    transform.position = new Vector3(transform.position.x + sideSpeed, 0, 0);
+
+                // tilt left
+                if (Input.gyro.attitude.x > 0.05 && transform.position.x > leftRoadBodundary)
+                    transform.position = new Vector3(transform.position.x - sideSpeed, 0, 0);
+            }
         }
     }
 }
